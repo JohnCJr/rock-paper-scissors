e21b412f2f8fa82c613c2572666d7cab7d9ad806
John <103902630+JohnCJr@users.noreply.github.com>
2023-07-21 21:29:04 -0400

    Added key functions that will be needed
    The key functions that will perform the majority of the tasks have been
    declared. As of now, they are empty bu the purpose behind each was Added
    as a comment to each function. Any array was also added and filled with
    values.

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+// As of this moment, the game will take place exclusively within the console
+
+// array that stores the possible choice either player can make
+let choices = ["rock", "paper", "scissors"];
+
+// function that gets computer choice of either Rock, Paper, or Scissors
+function computerSelection() {
+  return choices[Math.floor(Math.random() * 3)];
+}
+
+// function that takes user input to make a choice
+function playerSelection() {}
+
+// function that takes in the player and computer choices and returns a result
+function playRound(playerSelection, computerSelection) {}
+
+// function that loops through a round based on the parameter entered
+function game(rounds) {}
