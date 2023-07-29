306466a0cb2eeeffa9844d101a0e61fe1fb39a64
John <103902630+JohnCJr@users.noreply.github.com>
2023-07-29 13:41:18 -0400

    Created function and variables to update score
    A function, updateScore, will be used to update the current score. The
    The winDisplay and lossDisplay variables grants us access to the html
    section containing the winning and losing scores. Two other variables,
    winScore and lossScore, are used to store the running scores and are
    incremented by 1 within the playRound function. Those two variables are
    passed as parameters for the updateScore function, which is called
    within the playRound function as well.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 
 // array that stores the possible choice either player can make
 let choices = ["rock", "paper", "scissors"];
+let winScore = 0;
+let lossScore = 0;
 
 // function that gets computer choice of either Rock, Paper, or Scissors
 function computerSelection() {
@@ -31,28 +33,35 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock") {
     if (computerSelection === "paper") {
       result = "You lose. Paper beats Rock.";
+      lossScore++;
     } else if (computerSelection === "scissors") {
       result = "You win! Rock beats Scissors.";
+      winScore++;
     } else {
       result = "It's a draw between Rock and Rock.";
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
       result = "You win! Paper beats Rock.";
+      winScore++;
     } else if (computerSelection === "scissors") {
       result = "You lose. Scissors beats Paper.";
+      lossScore++;
     } else {
       result = "It's a draw between Paper and Paper.";
     }
   } else {
     if (computerSelection === "rock") {
       result = "You lose. Rock beats Scissors.";
+      lossScore++;
     } else if (computerSelection === "paper") {
       result = "You win! Scissors beats Paper.";
+      winScore++;
     } else {
       result = "It's a draw between Scissors and Scissors.";
     }
   }
+  updateScore(winScore, lossScore);
   return result;
 }
 
@@ -80,8 +89,19 @@ function playRound(playerSelection, computerSelection) {
 const buttons = document.querySelectorAll("button");
 const resultDisplay = document.querySelector(".resultBox");
 const roundResult = document.createElement("p");
+const winDisplay = document.querySelector(".winScore");
+const lossDisplay = document.querySelector(".lossScore");
 
 // console.log(buttons);
+
+// function addResultStyle(style) {
+//   resultDisplay.classList.add(`.${style}`);
+// }
+
+function updateScore(winScore, lossScore) {
+  winDisplay.textContent = winScore;
+  lossDisplay.textContent = lossScore;
+}
 
 buttons.forEach((button) =>
   button.addEventListener("click", () => {
